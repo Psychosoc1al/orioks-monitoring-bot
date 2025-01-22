337c1df96f057f4c72d68db1b804fd2b8a1d90b2
Psychosoc1al <tohichpetrov@gmail.com>
2025-01-22 22:46:21 +0300

    refactor(CommonHelper): use more Pythonic `try - return`

--- app/helpers/CommonHelper.py
+++ app/helpers/CommonHelper.py
@@ -51,9 +51,10 @@ class CommonHelper:
     def is_correct_convert_to_float(x) -> bool:
         try:
             float(x)
-            return True
         except ValueError:
             return False
+        else:
+            return True
 
     @staticmethod
     def safe_delete(path: str | pathlib.Path) -> None:
