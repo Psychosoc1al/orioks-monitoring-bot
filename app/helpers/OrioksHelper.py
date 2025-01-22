8cd63bdb4588c3bfab7dcc4a2dabbf8857198555
Psychosoc1al <tohichpetrov@gmail.com>
2025-01-22 23:03:33 +0300

    refactor(OrioksHelper): get rid of useless `try - except`s

--- app/helpers/OrioksHelper.py
+++ app/helpers/OrioksHelper.py
@@ -28,36 +28,31 @@ class OrioksHelper:
                 timeout=config.REQUESTS_TIMEOUT,
                 headers=config.ORIOKS_REQUESTS_HEADERS,
             ) as session:
-                try:
-                    logging.info(
-                        'request to login: %s',
-                        datetime.now().strftime("%H:%M:%S %d.%m.%Y"),
+                logging.info(
+                    'request to login: %s',
+                    datetime.now().strftime("%H:%M:%S %d.%m.%Y"),
+                )
+                async with session.get(
+                    str(config.ORIOKS_PAGE_URLS['login'])
+                ) as resp:
+                    bs_content = BeautifulSoup(
+                        await resp.text(), "html.parser"
                     )
-                    async with session.get(
-                        str(config.ORIOKS_PAGE_URLS['login'])
-                    ) as resp:
-                        bs_content = BeautifulSoup(
-                            await resp.text(), "html.parser"
-                        )
-                    _csrf_token = bs_content.find('input', {'name': '_csrf'})[
-                        'value'
-                    ]
-                    login_data = {
-                        'LoginForm[login]': int(user_login),
-                        'LoginForm[password]': str(user_password),
-                        'LoginForm[rememberMe]': 1,
-                        '_csrf': _csrf_token,
-                    }
-                except asyncio.TimeoutError as exception:
-                    raise exception
-                try:
-                    async with session.post(
-                        str(config.ORIOKS_PAGE_URLS['login']), data=login_data
-                    ) as resp:
-                        if str(resp.url) == config.ORIOKS_PAGE_URLS['login']:
-                            raise OrioksInvalidLoginCredentialsError
-                except asyncio.TimeoutError as exception:
-                    raise exception
+                _csrf_token = bs_content.find('input', {'name': '_csrf'})[
+                    'value'
+                ]
+                login_data = {
+                    'LoginForm[login]': int(user_login),
+                    'LoginForm[password]': str(user_password),
+                    'LoginForm[rememberMe]': 1,
+                    '_csrf': _csrf_token,
+                }
+
+                async with session.post(
+                    str(config.ORIOKS_PAGE_URLS['login']), data=login_data
+                ) as resp:
+                    if str(resp.url) == config.ORIOKS_PAGE_URLS['login']:
+                        raise OrioksInvalidLoginCredentialsError
 
                 cookies = session.cookie_jar.filter_cookies(resp.url)
 
